7e11b4efe9e3af6db22cee08cce5d799162fa120
Angelo Perera <angelo.perera@anz.com>
2019-10-27 10:45:47 +1100

    Re-implementing in GO

---
+++ stock.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+func get_max_profit() []int {
+
+	prices := []int{10, 7, 12, 5, 8, 11, 9}
+
+	fmt.Println("Prices", prices)
+
+	best_buy := prices[0]
+	best_sell := prices[1]
+
+	candidate_buy := 9999999 //math.Inf(1)
+	candidate_sell := -1     //math.Inf(-1)
+
+	for index, price := range prices {
+		fmt.Println("price", price, index)
+
+		if price < candidate_buy {
+			candidate_buy = price
+			candidate_sell = -1 //math.Inf(-1)
+		}
+
+		if price > candidate_sell {
+			candidate_sell = price
+		}
+
+		if ((candidate_sell / candidate_buy) > (best_sell / best_buy)) && (candidate_buy != candidate_sell) {
+			best_buy = candidate_buy
+			best_sell = candidate_sell
+		}
+	}
+
+	fmt.Println("Solution", best_buy, best_sell)
+
+	return []int{best_buy, best_sell}
+
+}
+
+func main() {
+	get_max_profit()
+}
